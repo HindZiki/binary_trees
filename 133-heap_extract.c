7feeae98d21e268682bcac78e244644f00127ded
Aosama <ahmed.osama@elsewedyintel.com>
2024-03-28 02:54:04 +0200

    heap extract

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - function extracts root node of Max Binary Heap
+* @root: double pointer to root node of heap
+* Return: value stored in root node
+*/
+
+int heap_extract(heap_t **root)
+{
+	printf("%d", (*root)->n);
+	return (0);
+}
